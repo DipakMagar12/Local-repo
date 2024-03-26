7ad33eced1f41f45dc61743d315972df50dd37c5
DipakMagar12 <dipakmagar12799@gmail.com>
2024-03-26 16:16:22 +0530

    hello

--- hello.js
+++ hello.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class hello extends Component {
   render() {
     return (
+      <div>
       <div>hello</div>
+      <h2>Heiii</h2>
+      </div>
     )
   }
 }
